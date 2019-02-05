42e9bb19007a6222a1ffc8b6cac84a9e74bbe934
AHoliak <42658600+AHoliak@users.noreply.github.com>
2019-02-04 19:51:48 -0800

    Add files via upload

---
+++ part3.js
@@ -0,0 +1,45 @@
+function numbers()
+{
+	var number1, number2, number3, number4, number5, n1, n2, n3, n4, n5;
+
+	number1 = document.forms["myform"].elements["num1"].value;
+	number2 = document.forms["myform"].elements["num2"].value;
+	number3 = document.forms["myform"].elements["num3"].value;
+	number4 = document.forms["myform"].elements["num4"].value;
+	number5 = document.forms["myform"].elements["num5"].value;
+
+	n1 = parseInt(number1);
+	n2 = parseInt(number2);
+	n3 = parseInt(number3);
+	n4 = parseInt(number4);
+	n5 = parseInt(number5);
+		
+	var i = 0;
+	var numbers = [n1, n2, n3, n4, n5];
+	var len = numbers.length;
+	var positive = 0;
+	var negative = 0;
+	var zeros = 0;
+
+	
+	for( i; i< len; i++)
+	{
+		if (numbers[i] > 0)
+		{
+			positive += 1;
+		} 
+		else if (numbers[i] < 0) 
+		{
+			negative += 1;
+		} 
+		else if (numbers[i] == 0)
+		{
+			zeros += 1;
+		}
+	}
+
+	document.forms["myform"].elements["result"].value  = (
+		"Negative numbers: " + negative + 
+		"\nPositive numbers: " + positive + 
+		"\nZero numbers: " + zeros );
+}
---
+++ part4.js
@@ -0,0 +1,23 @@
+var n1 = 1;
+
+	document.write( "<table class=\"main\">");
+
+	document.write( "<tr class=\"headers\"><th>Number</th><th>Square</th><th>Cube</th></tr>");
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	n1 += 1;
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	n1 += 1;
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	n1 += 1;
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	n1 += 1;
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	n1 += 1;
+
+	document.write( "<tr><td>" + n1 + "</td><td>" + Math.pow(n1,2) + "</td><td>" + Math.pow(n1,3) +"</td></tr>");
+	document.write( "</table>");
